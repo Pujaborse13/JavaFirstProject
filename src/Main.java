d3aa27ffbe86695862bfb23f9badd96174076f9e
Puja Borse <pujaborse13@gmail.com>
2024-11-22 17:21:52 +0530

    Addition

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)
+    {
+   int a = 15;
+   int b = 10;
+   int c = a+b;
+        System.out.println(c);
+
+
+        }
+    }
